1e0da3bd8466a7186bdb97ea7f56fa99f352494e
tmcnulty387 <tmcnulty387@gmail.com>
2025-03-18 11:07:57 -0400

    Added CLI template

---
+++ cli.py
@@ -0,0 +1,39 @@
+# register <username> <password> <first_name> <last_name> <email>
+
+# login <username> <password>
+
+# follow <email>
+
+# unfollow <email>
+
+# search_movies --title "Movie Name"
+# search_movies --release_date YYYY-MM-DD
+# search_movies --cast "Actor Name"
+# search_movies --studio "Studio Name"
+# search_movies --genre "Genre"
+
+# sort_movies --by name|studio|genre|year --order asc|desc
+
+# watch_movie <movie_id>
+
+# rate_movie <movie_id> <rating>
+
+# create_collection <collection_name>
+
+# add_to_collection <collection_name> <movie_id>
+
+# remove_from_collection <collection_name> <movie_id>
+
+# list_collections
+
+# rename_collection <old_name> <new_name>
+
+# delete_collection <collection_name>
+
+# list_following
+
+# list_followers
+
+# list_users
+
+# list_movies
